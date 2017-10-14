ea178f01d94de9bdd43243806b00cf6a1aa1c7fa
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-10-13 19:27:32 -0700

    got pagination working. Time to populate projects.

--- script/paginate.js
+++ script/paginate.js
@@ -1,1 +1,29 @@
 'use strict'
+
+$('.pageOneButton').on('click', function(){
+  if ($('.blobfhsiContainer').attr('opacity','0')) {
+    $('.ghosTownContainer')
+    .animate({
+      opacity: 0,
+      zIndex: 0
+    }, 500);
+    $('.blobfhsiContainer')
+    .animate({
+      opacity: 1,
+      zIndex: 1
+    }, 500);
+  }
+});
+
+$('.pageTwoButton').on('click', function(){
+  $('.blobfhsiContainer')
+  .animate({
+    opacity: 0,
+    zIndex: 0
+  }, 500);
+  $('.ghosTownContainer')
+  .animate({
+    opacity: 1,
+    zIndex: 1
+  }, 500);
+})
