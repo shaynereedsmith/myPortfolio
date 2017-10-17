e0af9122a1a8fc3871dd3ec5e1cc086b74152cda
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-10-16 19:21:59 -0700

    Working with responsiveness. Should have committed more often. Oops.

--- script/navigation.js
+++ script/navigation.js
@@ -3,23 +3,23 @@
 $('#skills').on('click', function() {
   $('html, body').animate({
     scrollTop: $('.toSkills').offset().top
-  }, 1000, 'easeInOutBack');
+  }, 1500, 'easeOutQuint');
 });
 
 $('#about').on('click', function() {
   $('html, body').animate({
     scrollTop: $('.toAbout').offset().top
-  }, 1000, 'easeInOutBack');
+  }, 1500, 'easeOutQuint');
 })
 
 $('#projects').on('click', function() {
   $('html, body').animate({
     scrollTop: $('.toProjects').offset().top
-  }, 1000, 'easeInOutBack');
+  }, 1500, 'easeOutQuint');
 })
 
 $('#contact').on('click', function() {
   $('html, body').animate({
     scrollTop: $('.toContact').offset().top
-  }, 1000, 'easeInOutBack');
+  }, 1500, 'easeOutQuint');
 })
